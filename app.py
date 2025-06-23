71bb4b768282a1944c39e3bc422093014d6731e7
jindun619 <jindun619@naver.com>
2025-06-23 04:30:51 +0000

    local server code added

--- app.py
+++ app.py
@@ -45,3 +45,4 @@ def process_image():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True, host="127.0.0.1", port=5000) #use this instead when running locally
